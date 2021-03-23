41241f4cce5725e88511fcd7167558195bc4202f
Mateusz Bączkowski <baczas996@gmail.com>
2021-03-24 00:29:50 +0100

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 iris = datasets.load_iris()
 X = iris.data[:, [2, 3]]
 y = iris.target
-print('Etykiety klas: ', np.unique(y))
+print('Class labels: ', np.unique(y))
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
 
@@ -24,16 +24,16 @@ ppn = Perceptron(max_iter=6, eta0=0.1, random_state=1)
 ppn.fit(X_train_std, y_train)
 
 y_pred = ppn.predict(X_test_std)
-print('Nieprawidłowo sklasyfikowane probki: %d' % (y_test != y_pred).sum())
+print('Incorrectly classified samples: %d' % (y_test != y_pred).sum())
 
-print('Dokładność: %.2f' % accuracy_score(y_test, y_pred))
-print('Dokładność: %.2f' % ppn.score(X_test_std, y_test)) #ta metoda wwdług mnie jest lepsza
+print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
+print('Accuracy: %.2f' % ppn.score(X_test_std, y_test)) 
 
 
 X_combined_std = np.vstack((X_train, X_test_std))
 y_combined = np.hstack((y_train, y_test))
 plot_decision_regions(X=X_combined_std, y=y_combined, classifier=ppn, test_idx=range(105, 150))
-plt.xlabel('długość płatka standaryzowana')
-plt.ylabel('szerokośc platka standaryzowana')
+plt.xlabel('Standardized sepal length in cm')
+plt.ylabel('Standardized petal length in cm')
 plt.legend(loc='upper left')
 plt.show()
